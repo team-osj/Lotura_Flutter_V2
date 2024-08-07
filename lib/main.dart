73fa3bd152465a74f8d974323c4a4e6ef092f902
유지우 <treeyu12345@naver.com>
2024-08-07 21:38:42 +0900

    SETTING :: ProviderScope 적용

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:lotura_v2/core/di/lotura_router.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    const ProviderScope(
+      child: MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
