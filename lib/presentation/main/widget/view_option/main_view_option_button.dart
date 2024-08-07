b7f4d0b17bdae2549d0153e77caee1f760605b4b
유지우 <treeyu12345@naver.com>
2024-08-07 18:35:44 +0900

    ANNO :: 주석 처리

--- lib/presentation/main/widget/view_option/main_view_option_button.dart
+++ lib/presentation/main/widget/view_option/main_view_option_button.dart
@@ -3,7 +3,9 @@ import 'package:lotura_v2/core/constants/lotura_color.dart';
 import 'package:lotura_v2/core/constants/lotura_text_style.dart';
 
 class MainViewOptionButton extends StatelessWidget {
+  /// 텍스트
   final String title;
+  /// Switch 되었는지 판단
   final bool isSwitched;
 
   const MainViewOptionButton({
@@ -15,9 +17,11 @@ class MainViewOptionButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SizedBox(
+      /// 옵션 별로 텍스트 길이가 각기 달라, 수치화 적용
       width: 79,
       child: Text(
         title,
+        /// 텍스트 가운데 정렬
         textAlign: TextAlign.center,
         style: LoturaTextStyle.body2(
           color: isSwitched ? LoturaColor.main900 : LoturaColor.black,
