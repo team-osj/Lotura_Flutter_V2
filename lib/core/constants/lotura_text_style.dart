61d25199fc40fe5a28d3f6a7a16e8a0c30c7ffc2
유지우 <treeyu12345@naver.com>
2024-08-07 18:22:25 +0900

    SETTING :: Lotura 텍스트 스타일 추가

---
+++ lib/core/constants/lotura_text_style.dart
@@ -0,0 +1,187 @@
+import 'package:flutter/material.dart';
+
+const pretendard = "Pretendard";
+
+class LoturaTextStyle {
+  LoturaTextStyle._();
+
+  static TextStyle caption({
+    required Color color,
+    TextOverflow overflow = TextOverflow.visible,
+  }) {
+    return TextStyle(
+      color: color,
+      fontSize: 12,
+      fontWeight: FontWeight.w400,
+      fontFamily: pretendard,
+      overflow: overflow,
+    );
+  }
+
+  static TextStyle label({
+    required Color color,
+    TextOverflow overflow = TextOverflow.visible,
+  }) {
+    return TextStyle(
+      color: color,
+      fontSize: 12,
+      fontWeight: FontWeight.w500,
+      fontFamily: pretendard,
+      overflow: overflow,
+    );
+  }
+
+  static TextStyle heading1({
+    required Color color,
+    TextOverflow overflow = TextOverflow.visible,
+  }) {
+    return TextStyle(
+        color: color,
+        fontSize: 42,
+        fontWeight: FontWeight.w600,
+        fontFamily: pretendard,
+        overflow: overflow);
+  }
+
+  static TextStyle heading2({
+    required Color color,
+    TextOverflow overflow = TextOverflow.visible,
+  }) {
+    return TextStyle(
+        color: color,
+        fontSize: 32,
+        fontWeight: FontWeight.w600,
+        fontFamily: pretendard,
+        overflow: overflow);
+  }
+
+  static TextStyle heading3({
+    required Color color,
+    TextOverflow overflow = TextOverflow.visible,
+  }) {
+    return TextStyle(
+      color: color,
+      fontSize: 24,
+      fontWeight: FontWeight.w600,
+      fontFamily: pretendard,
+      overflow: overflow,
+    );
+  }
+
+  static TextStyle heading4({
+    required Color color,
+    TextOverflow overflow = TextOverflow.visible,
+  }) {
+    return TextStyle(
+      color: color,
+      fontSize: 20,
+      fontWeight: FontWeight.w600,
+      fontFamily: pretendard,
+      overflow: overflow,
+    );
+  }
+
+  static TextStyle subTitle1({
+    required Color color,
+    TextOverflow overflow = TextOverflow.visible,
+  }) {
+    return TextStyle(
+      color: color,
+      fontSize: 18,
+      fontWeight: FontWeight.w600,
+      fontFamily: pretendard,
+      overflow: overflow,
+    );
+  }
+
+  static TextStyle subTitle2({
+    required Color color,
+    TextOverflow overflow = TextOverflow.visible,
+  }) {
+    return TextStyle(
+      color: color,
+      fontSize: 16,
+      fontWeight: FontWeight.w600,
+      fontFamily: pretendard,
+      overflow: overflow,
+    );
+  }
+
+  static TextStyle subTitle3({
+    required Color color,
+    TextOverflow overflow = TextOverflow.visible,
+  }) {
+    return TextStyle(
+      color: color,
+      fontSize: 14,
+      fontWeight: FontWeight.w600,
+      fontFamily: pretendard,
+      overflow: overflow,
+    );
+  }
+
+  static TextStyle body1({
+    required Color color,
+    TextOverflow overflow = TextOverflow.visible,
+  }) {
+    return TextStyle(
+      color: color,
+      fontSize: 14,
+      fontWeight: FontWeight.w500,
+      fontFamily: pretendard,
+      overflow: overflow,
+    );
+  }
+
+  static TextStyle body2({
+    required Color color,
+    TextOverflow overflow = TextOverflow.visible,
+  }) {
+    return TextStyle(
+      color: color,
+      fontSize: 12,
+      fontWeight: FontWeight.w500,
+      fontFamily: pretendard,
+      overflow: overflow,
+    );
+  }
+
+  static TextStyle body3({
+    required Color color,
+    TextOverflow overflow = TextOverflow.visible,
+  }) {
+    return TextStyle(
+      color: color,
+      fontSize: 10,
+      fontWeight: FontWeight.w500,
+      fontFamily: pretendard,
+      overflow: overflow,
+    );
+  }
+
+  static TextStyle button1({
+    required Color color,
+    TextOverflow overflow = TextOverflow.visible,
+  }) {
+    return TextStyle(
+      color: color,
+      fontSize: 16,
+      fontWeight: FontWeight.w500,
+      fontFamily: pretendard,
+      overflow: overflow,
+    );
+  }
+
+  static TextStyle button2({
+    required Color color,
+    TextOverflow overflow = TextOverflow.visible,
+  }) {
+    return TextStyle(
+      color: color,
+      fontSize: 12,
+      fontWeight: FontWeight.w600,
+      fontFamily: pretendard,
+      overflow: overflow,
+    );
+  }
+}
