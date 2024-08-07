10c43e0ba90d89c899dbb4970e5f53930fb0be89
유지우 <treeyu12345@naver.com>
2024-08-07 18:36:10 +0900

    ENCHANT :: 변수명 수정

--- lib/presentation/main/widget/view_option/main_view_option_button.dart
+++ lib/presentation/main/widget/view_option/main_view_option_button.dart
@@ -4,13 +4,13 @@ import 'package:lotura_v2/core/constants/lotura_text_style.dart';
 
 class MainViewOptionButton extends StatelessWidget {
   /// 텍스트
-  final String title;
+  final String text;
   /// Switch 되었는지 판단
   final bool isSwitched;
 
   const MainViewOptionButton({
     super.key,
-    required this.title,
+    required this.text,
     required this.isSwitched,
   });
 
@@ -20,7 +20,7 @@ class MainViewOptionButton extends StatelessWidget {
       /// 옵션 별로 텍스트 길이가 각기 달라, 수치화 적용
       width: 79,
       child: Text(
-        title,
+        text,
         /// 텍스트 가운데 정렬
         textAlign: TextAlign.center,
         style: LoturaTextStyle.body2(
