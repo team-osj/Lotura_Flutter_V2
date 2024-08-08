d1462d9605610266163de7cdcfcbd85daf6bba1c
유지우 <treeyu12345@naver.com>
2024-08-08 19:29:56 +0900

    ENCHANT :: ViewOptionProvider 코드 간소화

--- lib/presentation/main/provider/view_option_state_provider.dart
+++ lib/presentation/main/provider/view_option_state_provider.dart
@@ -1,9 +1,3 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-final viewOptionStateProvider = StateProvider<int>((ref) => 0);
-
-class ViewOptionController {
-  final int pageIndex;
-
-  ViewOptionController({required this.pageIndex});
-}
+final viewOptionStateProvider = StateProvider<int>((ref) => 1);
