f15b70d77cc8a6a8e6d793ed6a2fd44f78afc22f
유지우 <treeyu12345@naver.com>
2024-08-12 23:20:02 +0900

    FEAT :: 설정 스크린 메인 세탁실 설정 버튼 문서 추가

---
+++ lib/presentation/setting/widget/setting_laundry_locate_widget.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SettingLaundryLocateWidget extends StatelessWidget {
+  const SettingLaundryLocateWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
