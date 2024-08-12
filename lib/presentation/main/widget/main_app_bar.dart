478227ee08f8c3331c8447d8d96f2e481a9a176b
유지우 <treeyu12345@naver.com>
2024-08-12 23:23:57 +0900

    ENCHANT :: 라우터 설정

--- lib/presentation/main/widget/main_app_bar.dart
+++ lib/presentation/main/widget/main_app_bar.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:go_router/go_router.dart';
 import 'package:lotura_v2/core/constants/lotura_asset.dart';
 import 'package:lotura_v2/core/constants/lotura_color.dart';
 import 'package:lotura_v2/presentation/main/widget/view_option/main_view_option_switch.dart';
@@ -15,8 +16,10 @@ class MainAppBar extends StatelessWidget implements PreferredSizeWidget {
         child: AppBar(
           /// 자동적으로 leading 버튼이 생성되지 않도록 설정
           automaticallyImplyLeading: false,
+
           /// 스크롤할 때 AppBar 밑에 Elevation이 생기지 않도록 설정
           scrolledUnderElevation: 0,
+
           /// title property 사용 시, 양 옆 padding 제거
           titleSpacing: 0,
           backgroundColor: LoturaColor.white,
@@ -28,9 +31,15 @@ class MainAppBar extends StatelessWidget implements PreferredSizeWidget {
                 const MainViewOptionSwitch(),
                 Row(
                   children: [
-                    SvgPicture.asset("$coreAsset/bell_icon.svg"),
+                    GestureDetector(
+                      onTap: () => context.push("/notice"),
+                      child: SvgPicture.asset("$iconCoreAsset/bell_icon.svg"),
+                    ),
                     const SizedBox(width: 12),
-                    SvgPicture.asset("$coreAsset/setting_icon.svg"),
+                    GestureDetector(
+                      onTap: () => context.push("/setting"),
+                      child: SvgPicture.asset("$iconCoreAsset/setting_icon.svg"),
+                    ),
                   ],
                 )
               ],
