80d26a2aa39ad8fe517b708f9aa983e489201566
유지우 <treeyu12345@naver.com>
2024-08-12 23:22:54 +0900

    SETTING :: Hive 라이브러리 초기화

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:lotura_v2/core/di/lotura_router.dart';
 
-void main() {
+void main() async {
+  await Hive.initFlutter();
   runApp(
     const ProviderScope(
       child: MyApp(),
