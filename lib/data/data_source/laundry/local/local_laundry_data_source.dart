93b267bfe649e313f65d3f21c5545bdfa5cd8848
유지우 <treeyu12345@naver.com>
2024-09-09 11:49:24 +0900

    FEAT :: LocalLaundryDataSource 추가

---
+++ lib/data/data_source/laundry/local/local_laundry_data_source.dart
@@ -0,0 +1,28 @@
+import 'package:hive/hive.dart';
+import 'package:lotura_v2/core/provider/state/laundry/laundry_room_option.dart';
+
+class LocalLaundryDataSource {
+  late final Box<LaundryRoomOption> _laundryBox;
+
+  Future<void> init({required String key}) async {
+    if (!Hive.isBoxOpen("laundryRoomOption")) {
+      _laundryBox = await Hive.openBox<LaundryRoomOption>("laundryRoomOption");
+    }
+  }
+
+  Future<void> updateRoomOption({
+    required String key,
+    required LaundryRoomOption value,
+  }) async {
+    await init(key: key);
+    return _laundryBox.put(key, value);
+  }
+
+  Future<LaundryRoomOption?> getRoomOption({
+    required String key,
+  }) async {
+    await init(key: key);
+    return _laundryBox.get(key);
+  }
+}
+
