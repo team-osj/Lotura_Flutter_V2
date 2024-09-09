278c472e99ce6040aa8e9814848821a3fec1efab
유지우 <treeyu12345@naver.com>
2024-09-09 11:51:37 +0900

    FEAT :: GetLaundryRoomOptionViewModel 추가

---
+++ lib/presentation/setting/provider/view_model/get_laundry_room_option_view_model.dart
@@ -0,0 +1,43 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:lotura_v2/core/provider/state/laundry/laundry_room_option.dart';
+import 'package:lotura_v2/domain/entity/laundry/laundry_room_option_entity.dart';
+import 'package:lotura_v2/domain/use_case/laundry/get_laundry_room_option_use_case.dart';
+import 'package:lotura_v2/presentation/setting/provider/state/get_laundry_room_option_state.dart';
+
+class GetLaundryRoomOptionViewModel
+    extends StateNotifier<LaundryRoomOptionEntity<GetLaundryRoomOptionState>> {
+  final GetLaundryRoomOptionUseCase _getLaundryRoomOptionUseCase;
+
+  GetLaundryRoomOptionViewModel(
+      GetLaundryRoomOptionUseCase getLaundryRoomOptionUseCase)
+      : _getLaundryRoomOptionUseCase = getLaundryRoomOptionUseCase,
+        super(
+          LaundryRoomOptionEntity<GetLaundryRoomOptionState>(
+            state: GetLaundryRoomOptionState.initial,
+            option: null,
+          ),
+        );
+
+  Future<void> execute() async {
+    try {
+      state = state.copyWith(state: GetLaundryRoomOptionState.loading);
+
+      final response = await _getLaundryRoomOptionUseCase.execute();
+
+      switch (response) {
+        case LaundryRoomOption.maleDormitory:
+          state = state.copyWith(option: "남자 기숙사측");
+        case LaundryRoomOption.female:
+          state = state.copyWith(option: "여자 기숙사측");
+        default:
+          state = state.copyWith(option: "남자 학교측");
+      }
+
+      state = state.copyWith(state: GetLaundryRoomOptionState.success);
+    } catch (err) {
+      state = state.copyWith(state: GetLaundryRoomOptionState.failure);
+
+      throw Exception(err.toString());
+    }
+  }
+}
