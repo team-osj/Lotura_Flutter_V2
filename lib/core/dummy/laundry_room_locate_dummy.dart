6c89064fce0208f61441cce0272bb9c60f6809b2
유지우 <treeyu12345@naver.com>
2024-10-13 23:42:08 +0900

    ENCHANT :: 세탁실 위치 정보 수정

--- lib/core/dummy/laundry_room_locate_dummy.dart
+++ lib/core/dummy/laundry_room_locate_dummy.dart
@@ -1,51 +1,59 @@
-import 'package:lotura_v2/core/provider/state/device/device_type.dart';
+import 'package:lotura_v2/core/provider/state/device/device_array_type.dart';
 
-Map<int, DeviceType> maleSchoolLocateDummy = {
-  39: DeviceType.dryer,
-  1: DeviceType.dryer,
-  9: DeviceType.dryer,
-  41: DeviceType.dryer,
-  2: DeviceType.washer,
-  10: DeviceType.washer,
-  3: DeviceType.washer,
-  11: DeviceType.washer,
-  4: DeviceType.washer,
-  12: DeviceType.washer,
-  5: DeviceType.washer,
-  13: DeviceType.washer,
-  6: DeviceType.washer,
-  14: DeviceType.washer,
-  7: DeviceType.washer,
-  15: DeviceType.dryer,
-  47: DeviceType.dryer,
-  8: DeviceType.washer,
-  16: DeviceType.dryer,
-  48: DeviceType.dryer,
-};
+List<Map<DeviceArrayType, List<int>>> maleSchoolLocateDummy = [
+  {
+    DeviceArrayType.onlyDryer: [39, 1, 9, 41]
+  },
+  {
+    DeviceArrayType.onlyWasher: [2, 10]
+  },
+  {
+    DeviceArrayType.onlyWasher: [3, 11]
+  },
+  {
+    DeviceArrayType.onlyWasher: [4, 12]
+  },
+  {
+    DeviceArrayType.onlyWasher: [5, 13]
+  },
+  {
+    DeviceArrayType.onlyWasher: [6, 14]
+  },
+  {
+    DeviceArrayType.leftWasherRightDryer: [7, 15, 47]
+  },
+  {
+    DeviceArrayType.leftWasherRightDryer: [8, 16, 48]
+  },
+];
 
-Map<int, DeviceType> maleDormitoryLocateDummy = {
-  49: DeviceType.dryer,
-  17: DeviceType.dryer,
-  25: DeviceType.dryer,
-  57: DeviceType.dryer,
-  18: DeviceType.washer,
-  26: DeviceType.washer,
-  19: DeviceType.washer,
-  27: DeviceType.washer,
-  20: DeviceType.washer,
-  28: DeviceType.washer,
-  21: DeviceType.washer,
-  29: DeviceType.washer,
-  22: DeviceType.washer,
-  30: DeviceType.washer,
-  55: DeviceType.dryer,
-  23: DeviceType.dryer,
-  31: DeviceType.dryer,
-  63: DeviceType.dryer,
-  56: DeviceType.dryer,
-  24: DeviceType.dryer,
-};
+List<Map<DeviceArrayType, List<int>>> maleDormitoryLocateDummy = [
+  {
+    DeviceArrayType.onlyDryer: [49, 17, 25, 57]
+  },
+  {
+    DeviceArrayType.onlyWasher: [18, 26]
+  },
+  {
+    DeviceArrayType.onlyWasher: [19, 27]
+  },
+  {
+    DeviceArrayType.onlyWasher: [20, 28]
+  },
+  {
+    DeviceArrayType.onlyWasher: [21, 29]
+  },
+  {
+    DeviceArrayType.onlyWasher: [22, 30]
+  },
+  {
+    DeviceArrayType.onlyDryer: [55, 23, 31, 63]
+  },
+  {
+    DeviceArrayType.onlyDryer: [56, 24]
+  },
+];
 
-Map<int, DeviceType> femaleDormitoryLocateDummy = {
+List<Map<DeviceArrayType, List<int>>> femaleDormitoryLocateDummy = [
 
-};
+];
