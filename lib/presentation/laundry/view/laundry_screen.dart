a41ed25e184de641fb0f39f645c7f3a3db4f7d9a
유지우 <treeyu12345@naver.com>
2024-10-14 07:36:19 +0900

    FEAT :: 세탁기 & 건조기 배치

--- lib/presentation/laundry/view/laundry_screen.dart
+++ lib/presentation/laundry/view/laundry_screen.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:lotura_v2/core/constants/lotura_style.dart';
+import 'package:lotura_v2/core/dummy/laundry_room_locate_dummy.dart';
 import 'package:lotura_v2/core/layout/lotura_layout.dart';
 import 'package:lotura_v2/presentation/laundry/provider/local_laundry_room_option_provider.dart';
+import 'package:lotura_v2/presentation/laundry/widget/laundry_device_array_widget.dart';
 import 'package:lotura_v2/presentation/laundry/widget/laundry_room_select_radio_button.dart';
 
 class LaundryScreen extends ConsumerStatefulWidget {
@@ -29,10 +31,9 @@ class _LaundryScreenState extends ConsumerState<LaundryScreen> {
 
   @override
   Widget build(BuildContext context) {
-    final localLaundryRoomOption =
-        ref.watch(localLaundryRoomOptionViewModelProvider).option;
-    final updateLocalLaundryRoomOption =
-        ref.read(localLaundryRoomOptionViewModelProvider.notifier);
+    final localLaundryRoomOption = ref.watch(localLaundryRoomOptionViewModelProvider).option;
+    final localLaundryRoomLocate = ref.watch(localLaundryRoomOptionViewModelProvider).locate;
+    final updateLocalLaundryRoomOption = ref.read(localLaundryRoomOptionViewModelProvider.notifier);
     return LoturaLayout(
       child: SingleChildScrollView(
         child: Padding(
@@ -57,6 +58,7 @@ class _LaundryScreenState extends ConsumerState<LaundryScreen> {
                         if (localLaundryRoomOption != "남자 학교측") {
                           updateLocalLaundryRoomOption.changeOption(
                             option: "남자 학교측",
+                            locate: maleSchoolLocateDummy,
                           );
                         }
                       },
@@ -71,6 +73,7 @@ class _LaundryScreenState extends ConsumerState<LaundryScreen> {
                         if (localLaundryRoomOption != "남자 기숙사측") {
                           updateLocalLaundryRoomOption.changeOption(
                             option: "남자 기숙사측",
+                            locate: maleDormitoryLocateDummy,
                           );
                         }
                       },
@@ -85,6 +88,7 @@ class _LaundryScreenState extends ConsumerState<LaundryScreen> {
                         if (localLaundryRoomOption != "여자 기숙사측") {
                           updateLocalLaundryRoomOption.changeOption(
                             option: "여자 기숙사측",
+                            locate: femaleDormitoryLocateDummy,
                           );
                         }
                       },
@@ -96,20 +100,26 @@ class _LaundryScreenState extends ConsumerState<LaundryScreen> {
                 ),
               ),
               const SizedBox(height: 24),
-
-              // ListView.builder(
-              //   controller: _scrollController,
-              //   shrinkWrap: true,
-              //   physics: const NeverScrollableScrollPhysics(),
-              //   itemCount: maleDormitoryLocateDummy.length,
-              //   itemBuilder: (context, index) {
-              //     return Padding(
-              //       padding:
-              //           EdgeInsets.only(bottom: index == maleDormitoryLocateDummy.length - 1 ? 24 : 10),
-              //       child: LaundryDeviceArrayWidget(type: maleDormitoryLocateDummy.values.elementAt(index)),
-              //     );
-              //   },
-              // ),
+              ListView.builder(
+                controller: _scrollController,
+                shrinkWrap: true,
+                physics: const NeverScrollableScrollPhysics(),
+                itemCount: localLaundryRoomLocate!.length,
+                itemBuilder: (context, index) {
+                  return Padding(
+                    padding: EdgeInsets.only(
+                      bottom: index == localLaundryRoomLocate.length - 1
+                          ? 24
+                          : 10,
+                    ),
+                    child: LaundryDeviceArrayWidget(
+                      type: localLaundryRoomLocate.elementAt(index).keys.single,
+                      locate: maleDormitoryLocateDummy,
+                      item: localLaundryRoomLocate.elementAt(index).values.single,
+                    ),
+                  );
+                },
+              ),
             ],
           ),
         ),
--- lib/presentation/laundry/widget/laundry_device_array_widget.dart
+++ lib/presentation/laundry/widget/laundry_device_array_widget.dart
@@ -1,39 +1,131 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:lotura_v2/core/constants/lotura_style.dart';
+import 'package:lotura_v2/core/provider/state/device/device_array_type.dart';
 import 'package:lotura_v2/core/provider/state/device/device_state.dart';
 import 'package:lotura_v2/core/provider/state/device/device_type.dart';
+import 'package:lotura_v2/domain/entity/laundry/laundry_entity.dart';
 import 'package:lotura_v2/presentation/laundry/widget/laundry_device_state_widget.dart';
 import 'package:lotura_v2/presentation/laundry/widget/laundry_dryer_array_widget.dart';
+import 'package:lotura_v2/presentation/laundry/widget/laundry_empty_widget.dart';
 
 class LaundryDeviceArrayWidget extends StatelessWidget {
-  final DeviceType type;
+  final DeviceArrayType type;
+  final List<Map<DeviceArrayType, List<int>>> locate;
+  final List<int> item;
 
   const LaundryDeviceArrayWidget({
     super.key,
     required this.type,
+    required this.locate,
+    required this.item,
   });
 
   @override
   Widget build(BuildContext context) {
+    final int length = item.length;
+    List<Widget> children;
+
+    if (length == 4) {
+      children = [
+        LaundryDryerArrayWidget(
+          left: LaundryEntity(
+            id: item.elementAt(0),
+            state: DeviceState.available,
+            type: DeviceType.dryer,
+          ),
+          right: LaundryEntity(
+            id: item.elementAt(1),
+            state: DeviceState.available,
+            type: DeviceType.dryer,
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 33),
+          child: SvgPicture.asset("$iconLaundryAsset/movement_sign_icon.svg"),
+        ),
+        LaundryDryerArrayWidget(
+          left: LaundryEntity(
+            id: item.elementAt(2),
+            state: DeviceState.available,
+            type: DeviceType.dryer,
+          ),
+          right: LaundryEntity(
+            id: item.elementAt(3),
+            state: DeviceState.available,
+            type: DeviceType.dryer,
+          ),
+        ),
+      ];
+    } else if (length == 3) {
+      children = [
+        LaundryDeviceStateWidget(
+          id: item.elementAt(0),
+          type: DeviceType.washer,
+          state: DeviceState.available,
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 33),
+          child: SvgPicture.asset("$iconLaundryAsset/movement_sign_icon.svg"),
+        ),
+        LaundryDryerArrayWidget(
+          left: LaundryEntity(
+            id: item.elementAt(1),
+            state: DeviceState.available,
+            type: DeviceType.dryer,
+          ),
+          right: LaundryEntity(
+            id: item.elementAt(2),
+            state: DeviceState.available,
+            type: DeviceType.dryer,
+          ),
+        ),
+      ];
+    } else if (type == DeviceArrayType.onlyWasher) {
+      children = [
+        LaundryDeviceStateWidget(
+          id: item.elementAt(0),
+          type: DeviceType.washer,
+          state: DeviceState.available,
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 33),
+          child: SvgPicture.asset("$iconLaundryAsset/movement_sign_icon.svg"),
+        ),
+        LaundryDeviceStateWidget(
+          id: item.elementAt(1),
+          type: DeviceType.washer,
+          state: DeviceState.available,
+        ),
+      ];
+    } else {
+      children = [
+        LaundryDryerArrayWidget(
+          left: LaundryEntity(
+            id: item.elementAt(0),
+            state: DeviceState.available,
+            type: DeviceType.dryer,
+          ),
+          right: LaundryEntity(
+            id: item.elementAt(1),
+            state: DeviceState.available,
+            type: DeviceType.dryer,
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 33),
+          child: SvgPicture.asset("$iconLaundryAsset/movement_sign_icon.svg"),
+        ),
+        const LaundryEmptyWidget(),
+      ];
+    }
+
     return FittedBox(
       child: Row(
-        children: type == DeviceType.dryer ? [
-          const LaundryDryerArrayWidget(),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 33),
-            child: SvgPicture.asset("$iconLaundryAsset/movement_sign_icon.svg"),
-          ),
-          const LaundryDryerArrayWidget(),
-        ] : [
-          LaundryDeviceStateWidget(type: type, state: DeviceState.breakdown),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 33),
-            child: SvgPicture.asset("$iconLaundryAsset/movement_sign_icon.svg"),
-          ),
-          LaundryDeviceStateWidget(type: type, state: DeviceState.disconnect),
-        ],
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: children,
       ),
     );
   }
+
 }
