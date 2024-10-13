81654679c4bab94a89b80fa2fa0045309087dc20
유지우 <treeyu12345@naver.com>
2024-10-14 07:34:44 +0900

    FEAT :: 건조기 배치 위젯 구현

--- lib/presentation/laundry/widget/laundry_dryer_array_widget.dart
+++ lib/presentation/laundry/widget/laundry_dryer_array_widget.dart
@@ -1,18 +1,32 @@
 import 'package:flutter/material.dart';
-import 'package:lotura_v2/core/provider/state/device/device_state.dart';
 import 'package:lotura_v2/core/provider/state/device/device_type.dart';
+import 'package:lotura_v2/domain/entity/laundry/laundry_entity.dart';
 import 'package:lotura_v2/presentation/laundry/widget/laundry_device_state_widget.dart';
 
 class LaundryDryerArrayWidget extends StatelessWidget {
-  const LaundryDryerArrayWidget({super.key});
+  final LaundryEntity left, right;
+
+  const LaundryDryerArrayWidget({
+    super.key,
+    required this.left,
+    required this.right,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return const Row(
+    return Row(
       children: [
-        LaundryDeviceStateWidget(type: DeviceType.dryer, state: DeviceState.working),
-        SizedBox(width: 8),
-        LaundryDeviceStateWidget(type: DeviceType.dryer, state: DeviceState.working),
+        LaundryDeviceStateWidget(
+          type: DeviceType.dryer,
+          state: left.state,
+          id: left.id,
+        ),
+        const SizedBox(width: 8),
+        LaundryDeviceStateWidget(
+          type: DeviceType.dryer,
+          state: right.state,
+          id: right.id,
+        ),
       ],
     );
   }
