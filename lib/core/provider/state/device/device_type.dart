f52080ff5514907444b8976295a4aad2b2740e3c
유지우 <treeyu12345@naver.com>
2024-10-13 23:43:15 +0900

    REFACTOR :: 장치 종류를 나타내는 정보 구체화

--- lib/core/provider/state/device/device_type.dart
+++ lib/core/provider/state/device/device_type.dart
@@ -1,4 +1,27 @@
+import 'package:lotura_v2/core/constants/lotura_style.dart';
+
 enum DeviceType {
-  washer, /// 세탁기
-  dryer, /// 건조기
+  /// 세탁기
+  washer(
+    icon: "$iconLaundryAsset/dryer_icon.svg",
+    text: "세탁기",
+    padding: 55.5
+  ),
+
+  /// 건조기
+  dryer(
+    icon: "$iconLaundryAsset/washer_icon.svg",
+    text: "건조기",
+    padding: 16.5
+  );
+
+  final String icon;
+  final String text;
+  final double padding;
+
+  const DeviceType({
+    required this.icon,
+    required this.text,
+    required this.padding,
+  });
 }
