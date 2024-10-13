b7ce7a2d3fed224614af82c7beeb99a4fbc2b17b
유지우 <treeyu12345@naver.com>
2024-10-14 07:35:16 +0900

    FEAT :: 세탁기가 없는 구역을 표시하는 위젯 구현

---
+++ lib/presentation/laundry/widget/laundry_empty_widget.dart
@@ -0,0 +1,15 @@
+import 'dart:ffi';
+
+import 'package:flutter/cupertino.dart';
+
+class LaundryEmptyWidget extends StatelessWidget {
+  const LaundryEmptyWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const SizedBox(
+      height: 82,
+      width: 148,
+    );
+  }
+}
