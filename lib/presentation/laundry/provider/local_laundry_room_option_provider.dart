a68517c36d3d420b60f6e64a32aa61041ad0af1d
유지우 <treeyu12345@naver.com>
2024-10-13 23:46:31 +0900

    FEAT :: 위치 데이터 추가

--- lib/presentation/laundry/provider/local_laundry_room_option_provider.dart
+++ lib/presentation/laundry/provider/local_laundry_room_option_provider.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:lotura_v2/core/dummy/laundry_room_locate_dummy.dart';
 import 'package:lotura_v2/domain/entity/laundry/laundry_room_option_entity.dart';
 import 'package:lotura_v2/presentation/laundry/provider/state/local_laundry_room_option_state.dart';
 import 'package:lotura_v2/presentation/laundry/provider/view_model/local_laundry_room_option_view_model.dart';
@@ -7,5 +8,12 @@ import 'package:lotura_v2/presentation/setting/provider/laundry/get_laundry_room
 final localLaundryRoomOptionViewModelProvider = StateNotifierProvider<
     LocalLaundryRoomOptionViewModel,
     LaundryRoomOptionEntity<LocalLaundryRoomOptionState>>(
-  (ref) => LocalLaundryRoomOptionViewModel(storedOption: ref.watch(getLaundryRoomOptionViewModelProvider).option ?? "남자 학교측"),
+  (ref) {
+    final localLaundryRoomOptionProvider =
+        ref.watch(getLaundryRoomOptionViewModelProvider);
+    return LocalLaundryRoomOptionViewModel(
+      storedOption: localLaundryRoomOptionProvider.option ?? "남자 학교측",
+      storedLocate: localLaundryRoomOptionProvider.locate ?? maleSchoolLocateDummy,
+    );
+  },
 );
--- lib/presentation/laundry/provider/view_model/local_laundry_room_option_view_model.dart
+++ lib/presentation/laundry/provider/view_model/local_laundry_room_option_view_model.dart
@@ -1,24 +1,34 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:lotura_v2/core/dummy/laundry_room_locate_dummy.dart';
+import 'package:lotura_v2/core/provider/state/device/device_array_type.dart';
 import 'package:lotura_v2/domain/entity/laundry/laundry_room_option_entity.dart';
 import 'package:lotura_v2/presentation/laundry/provider/state/local_laundry_room_option_state.dart';
 
-class LocalLaundryRoomOptionViewModel extends StateNotifier<LaundryRoomOptionEntity<LocalLaundryRoomOptionState>> {
-
-  LocalLaundryRoomOptionViewModel({required String storedOption})
-      : super(
-          LaundryRoomOptionEntity(
-            state: LocalLaundryRoomOptionState.initial,
-            option: storedOption,
-          ),
-        );
+class LocalLaundryRoomOptionViewModel extends StateNotifier<
+    LaundryRoomOptionEntity<LocalLaundryRoomOptionState>> {
+  LocalLaundryRoomOptionViewModel({
+    required String storedOption,
+    required List<Map<DeviceArrayType, List<int>>> storedLocate,
+  }) : super(
+    LaundryRoomOptionEntity(
+      state: LocalLaundryRoomOptionState.initial,
+      option: storedOption,
+      locate: storedLocate,
+    ),
+  );
 
   void changeOption({
     required String option,
+    required List<Map<DeviceArrayType, List<int>>> locate,
   }) {
     try {
       state = state.copyWith(state: LocalLaundryRoomOptionState.loading);
       if (option != state.option) {
-        state = state.copyWith(option: option, state: LocalLaundryRoomOptionState.success);
+        state = state.copyWith(
+            state: LocalLaundryRoomOptionState.success,
+            option: option,
+            locate: locate
+        );
       }
     } catch (err) {
       state = state.copyWith(state: LocalLaundryRoomOptionState.failure);
