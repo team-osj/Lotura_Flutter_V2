e7a726f0780a34e09898ef69c683201898c8ee1d
유지우 <treeyu12345@naver.com>
2024-10-15 22:45:42 +0900

    FEAT :: 세탁실 Repository 구현

--- lib/data/repository/laundry/laundry_repository_impl.dart
+++ lib/data/repository/laundry/laundry_repository_impl.dart
@@ -1,13 +1,29 @@
 import 'package:lotura_v2/core/hive/laundry_room_option.dart';
 import 'package:lotura_v2/data/data_source/laundry/local/local_laundry_data_source.dart';
+import 'package:lotura_v2/data/data_source/laundry/remote/remote_laundry_data_source.dart';
+import 'package:lotura_v2/domain/entity/laundry/laundry_entity.dart';
 import 'package:lotura_v2/domain/repository/laundry/laundry_repository.dart';
 
 class LaundryRepositoryImpl implements LaundryRepository {
   final LocalLaundryDataSource _localLaundryDataSource;
+  final RemoteLaundryDataSource _remoteLaundryDataSource;
 
   LaundryRepositoryImpl({
     required LocalLaundryDataSource localLaundryDataSource,
-  }) : _localLaundryDataSource = localLaundryDataSource;
+    required RemoteLaundryDataSource remoteLaundryDataSource,
+  })  : _localLaundryDataSource = localLaundryDataSource,
+        _remoteLaundryDataSource = remoteLaundryDataSource;
+
+  @override
+  Stream<LaundryEntity> get laundryList =>
+      _remoteLaundryDataSource.laundryList.asBroadcastStream();
+
+  @override
+  void initWebSocket() => _remoteLaundryDataSource.initWebSocket();
+
+  @override
+  Future<List<LaundryEntity>> getAllLaundryList() =>
+      _remoteLaundryDataSource.getAllLaundryList();
 
   @override
   Future<void> updateRoomOption({
