0b1a082952d36b9df5eda46ca84cc6a3bce686c2
유지우 <treeyu12345@naver.com>
2024-10-16 22:55:53 +0900

    ENCHANT :: 앱이 빌드될 때 우선적으로 불러와져야하는 데이터 요청

--- lib/presentation/splash/view/splash_screen.dart
+++ lib/presentation/splash/view/splash_screen.dart
@@ -3,6 +3,8 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:lotura_v2/core/constants/lotura_style.dart';
 import 'package:lotura_v2/presentation/laundry/provider/get_stream_laundry_view_model_provider.dart';
+import 'package:lotura_v2/presentation/notice/provider/get_notice_option_view_model_provider.dart';
+import 'package:lotura_v2/presentation/notice/provider/get_notice_view_model_provider.dart';
 import 'package:lotura_v2/presentation/setting/provider/laundry/get_laundry_room_option_view_model_provider.dart';
 
 class SplashScreen extends ConsumerStatefulWidget {
@@ -21,7 +23,12 @@ class _SplashScreenState extends ConsumerState<SplashScreen> {
       ref.read(getLaundryRoomOptionViewModelProvider.notifier).execute();
       /// 앱이 빌드될 떄 서버 소켓과 연결
       ref.read(getStreamLaundryViewModelProvider.notifier).getLaundryEvent();
+      /// 모든 장치의 상태를 GET 요청함
       ref.read(getStreamLaundryViewModelProvider.notifier).getAllLaundryList();
+      /// 읽은 공지사항 리스트를 불러옴
+      ref.read(getNoticeOptionViewModelProvider.notifier).execute();
+      /// 공지사항 전체 리스트를 GET 요청
+      ref.read(getNoticeViewModelProvider.notifier).execute();
       Future.delayed(const Duration(milliseconds: 1100)).then((value) => context.go("/"));
     });
   }
