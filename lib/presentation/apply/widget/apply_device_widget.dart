57d8f69a745090f95ce1773cedb3a2deaa4a987c
유지우 <treeyu12345@naver.com>
2024-10-17 10:09:59 +0900

    FEAT :: 세탁실 알림 신청 목록 위젯 퍼블리싱

--- lib/presentation/apply/widget/apply_device_widget.dart
+++ lib/presentation/apply/widget/apply_device_widget.dart
@@ -7,69 +7,54 @@ import 'package:lotura_v2/core/provider/state/device/device_type.dart';
 import 'package:lotura_v2/presentation/apply/widget/apply_device_state_widget.dart';
 
 class ApplyDeviceWidget extends StatelessWidget {
-  final int number;
+  final int id;
   final DeviceType type;
   final DeviceState state;
 
   const ApplyDeviceWidget({
     super.key,
-    required this.number,
+    required this.id,
     required this.type,
     required this.state,
   });
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(12),
-        border: Border.all(
-          width: 1,
-          color: LoturaColor.main50,
-          strokeAlign: BorderSide.strokeAlignInside,
+    return FittedBox(
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(12),
+          border: Border.all(
+            width: 1,
+            color: LoturaColor.main50,
+            strokeAlign: BorderSide.strokeAlignInside,
+          ),
         ),
-      ),
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 22, vertical: 20),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Image.asset(
-              "$imageCoreAsset/${type == DeviceType.dryer ? "dryer" : "washer"}_image.png",
-              width: 120,
-              height: 120,
-            ),
-            const SizedBox(height: 12),
-            Text(
-              "$number번 ${type == DeviceType.dryer ? "건조기" : "세탁기"}",
-              style: LoturaTextStyle.subTitle2(
-                color: LoturaColor.black,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 22, vertical: 20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Image.asset(
+                "$imageApplyAsset/${type.name}_image.png",
+                width: 120,
+                height: 120,
               ),
-            ),
-            const SizedBox(height: 12),
-            switch (state) {
-              DeviceState.available => const ApplyDeviceStateWidget(
-                  state: "사용 가능",
-                  color: LoturaColor.green20,
-                  backgroundColor: LoturaColor.green10,
+              const SizedBox(height: 12),
+              Text(
+                "$id번 ${type.text}",
+                style: LoturaTextStyle.subTitle2(
+                  color: LoturaColor.black,
                 ),
-              DeviceState.working => const ApplyDeviceStateWidget(
-                  state: "작동중",
-                  color: LoturaColor.main900,
-                  backgroundColor: LoturaColor.main50,
-                ),
-              DeviceState.disconnect => const ApplyDeviceStateWidget(
-                  state: "연결 끊김",
-                  color: LoturaColor.gray600,
-                  backgroundColor: LoturaColor.gray100,
-                ),
-              DeviceState.breakdown => const ApplyDeviceStateWidget(
-                  state: "고장",
-                  color: LoturaColor.red20,
-                  backgroundColor: LoturaColor.red10,
-                ),
-            },
-          ],
+              ),
+              const SizedBox(height: 12),
+              ApplyDeviceStateWidget(
+                state: state.text,
+                color: state.iconColor,
+                backgroundColor: state.color,
+              ),
+            ],
+          ),
         ),
       ),
     );
