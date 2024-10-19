22a797146c90a6cca3bdbec5aae7f3e3f1aac282
유지우 <treeyu12345@naver.com>
2024-10-19 22:06:23 +0900

    BURN :: 메인 세탁실 설정 관련 코드 삭제

--- lib/presentation/setting/view/setting_screen.dart
+++ lib/presentation/setting/view/setting_screen.dart
@@ -3,12 +3,10 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:lotura_v2/core/component/lotura_app_bar.dart';
 import 'package:lotura_v2/core/layout/lotura_layout.dart';
 import 'package:lotura_v2/presentation/setting/provider/laundry/get_laundry_room_option_view_model_provider.dart';
-import 'package:lotura_v2/presentation/setting/provider/window/get_window_option_view_model_provider.dart';
 import 'package:lotura_v2/presentation/setting/widget/alarm/setting_alarm_widget.dart';
 import 'package:lotura_v2/presentation/setting/widget/inquiry/setting_inquiry_widget.dart';
 import 'package:lotura_v2/presentation/setting/widget/laundry/setting_main_laundry_bottom_sheet.dart';
 import 'package:lotura_v2/presentation/setting/widget/setting_option_widget.dart';
-import 'package:lotura_v2/presentation/setting/widget/window/setting_main_window_bottom_sheet.dart';
 
 class SettingScreen extends ConsumerWidget {
   const SettingScreen({super.key});
@@ -16,7 +14,6 @@ class SettingScreen extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final laundryOption = ref.watch(getLaundryRoomOptionViewModelProvider);
-    final windowOption = ref.watch(getWindowOptionViewModelProvider);
     return LoturaLayout(
       appBar: const LoturaAppBar(
         popRoute: "/",
@@ -31,12 +28,6 @@ class SettingScreen extends ConsumerWidget {
               bottomSheet: const SettingMainLaundryBottomSheet(),
               title: "메인 세탁실 설정",
               option: laundryOption.option.toString(),
-            ),
-            const SizedBox(height: 12),
-            SettingOptionWidget(
-              bottomSheet: const SettingMainWindowBottomSheet(),
-              title: "메인 화면 설정",
-              option: windowOption.option.toString(),
             ),
             const SizedBox(height: 12),
             const SettingAlarmWidget(),
