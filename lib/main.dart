3cd687eb2a0906f407a835ef51dd4a33efbd4621
유지우 <treeyu12345@naver.com>
2024-10-19 22:05:24 +0900

    BURN :: Hive Adapter 삭제

--- lib/main.dart
+++ lib/main.dart
@@ -3,14 +3,12 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:lotura_v2/core/di/lotura_router.dart';
 import 'package:lotura_v2/core/hive/laundry_room_option.dart';
-import 'package:lotura_v2/core/hive/window_option.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Hive.initFlutter();
   /// Hive 어댑터 등록
   Hive.registerAdapter(LaundryRoomOptionAdapter());
-  Hive.registerAdapter(WindowOptionAdapter());
   runApp(
     const ProviderScope(
       child: MyApp(),
