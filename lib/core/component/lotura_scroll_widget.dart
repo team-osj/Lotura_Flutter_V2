feca18880082eb9615f8365be6e5cb7d22ed7c8e
유지우 <treeyu12345@naver.com>
2024-10-28 10:36:07 +0900

    ENCHANT :: 스크롤바 커스텀화

--- lib/core/component/lotura_scroll_widget.dart
+++ lib/core/component/lotura_scroll_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:lotura_v2/core/constants/lotura_style.dart';
 
 class LoturaScrollWidget extends StatelessWidget {
   final ScrollController controller;
@@ -14,9 +15,11 @@ class LoturaScrollWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scrollbar(
+    return RawScrollbar(
       controller: controller,
       thickness: 6,
+      thumbColor: LoturaColor.gray200,
+      crossAxisMargin: 6,
       radius: const Radius.circular(12),
       child: SingleChildScrollView(
         controller: controller,
