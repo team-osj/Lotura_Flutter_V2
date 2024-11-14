b35e51aa1344b65f90da404fcef43c2912e78777
유지우 <treeyu12345@naver.com>
2024-11-14 18:58:02 +0900

    FEAT :: 테마 옵션 업데이트 View Model 구현

---
+++ lib/core/theme/view_model/update_theme_option_view_model.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:lotura_v2/domain/use_case/theme/update_theme_option_use_case.dart';
+import 'package:lotura_v2/core/theme/view_model/state/update_theme_option_state.dart';
+
+class UpdateThemeOptionViewModel extends StateNotifier<UpdateThemeOptionState> {
+  final UpdateThemeOptionUseCase _updateThemeOptionUseCase;
+
+  UpdateThemeOptionViewModel({
+    required UpdateThemeOptionUseCase updateThemeOptionUseCase,
+  })  : _updateThemeOptionUseCase = updateThemeOptionUseCase,
+        super(UpdateThemeOptionState.initial);
+
+  Future<void> execute({
+    required Brightness brightness,
+  }) async {
+    try {
+      state = UpdateThemeOptionState.loading;
+
+      final mode = switch(brightness) {
+        Brightness.dark => ThemeMode.light,
+        Brightness.light => ThemeMode.dark,
+      };
+
+      await _updateThemeOptionUseCase.execute(mode: mode);
+
+      state = UpdateThemeOptionState.success;
+    } catch (err) {
+      state = UpdateThemeOptionState.failure;
+
+      throw Exception(err.toString());
+    }
+  }
+}
