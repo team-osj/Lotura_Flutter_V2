fc42e4cff42de78f9e90e4b92889d2af95af2b1d
유지우 <treeyu12345@naver.com>
2024-11-14 18:52:10 +0900

    REFACT :: 다크모드 적용으로 인한 장치 상태 관리 타입 구조 변경

--- lib/core/provider/state/device/device_state.dart
+++ lib/core/provider/state/device/device_state.dart
@@ -1,67 +1,54 @@
 import 'package:flutter/material.dart';
-import 'package:lotura_v2/core/constants/lotura_style.dart';
 
 enum DeviceState {
   /// 작동중
-  working(
-    color: LoturaLightColor.main50,
-    iconColor: LoturaLightColor.main500,
-    text: "작동중",
-  ),
+  working,
 
   /// 사용 가능
-  available(
-    color: LoturaLightColor.green10,
-    iconColor: LoturaLightColor.green20,
-    text: "사용 가능",
-  ),
+  available,
 
   /// 연결 끊김
-  disconnect(
-    color: LoturaLightColor.gray50,
-    iconColor: LoturaLightColor.gray500,
-    text: "연결 끊김",
-  ),
+  disconnect,
 
   /// 고장
-  breakdown(
-    color: LoturaLightColor.red10,
-    iconColor: LoturaLightColor.red20,
-    text: "고장",
-  );
+  breakdown;
 
-  final Color color;
-  final Color iconColor;
-  final String text;
+  Color themeColorHandler(BuildContext context) {
+    switch (this) {
+      case DeviceState.working:
+        return Theme.of(context).colorScheme.onTertiary; // 예시로 기본 색상 사용
+      case DeviceState.available:
+        return Theme.of(context).colorScheme.primaryContainer;
+      case DeviceState.disconnect:
+        return Theme.of(context).colorScheme.secondary;
+      case DeviceState.breakdown:
+        return Theme.of(context).colorScheme.onError;
+    }
+  }
 
-  const DeviceState({
-    required this.color,
-    required this.iconColor,
-    required this.text,
-  });
+  Color themeIconColorHandler(BuildContext context) {
+    switch (this) {
+      case DeviceState.working:
+        return Theme.of(context).colorScheme.tertiary;
+      case DeviceState.available:
+        return Theme.of(context).colorScheme.primary;
+      case DeviceState.disconnect:
+        return Theme.of(context).colorScheme.onSecondary;
+      case DeviceState.breakdown:
+        return Theme.of(context).colorScheme.error;
+    }
+  }
+
+  String themeTextHandler() {
+    switch (this) {
+      case DeviceState.working:
+        return "작동중";
+      case DeviceState.available:
+        return "사용 가능";
+      case DeviceState.disconnect:
+        return "연결 끊김";
+      case DeviceState.breakdown:
+        return "고장";
+    }
+  }
 }
-
-// Map<DeviceState, List<Color>> deviceColor = {
-//   DeviceState.available: [LoturaColor.green10, LoturaColor.green20],
-//   DeviceState.working: [LoturaColor.main50, LoturaColor.main500],
-//   DeviceState.disconnect: [LoturaColor.gray50, LoturaColor.gray500],
-//   DeviceState.breakdown: [LoturaColor.red10, LoturaColor.red20],
-// };
-//
-// Map<DeviceState, List<String>> deviceComment = {
-//   DeviceState.available: ["는\n현재 사용할 수 있어요!"],
-//   DeviceState.working: [
-//     "를\n알림 설정할까요?",
-//     "가 종료되면 알림을 드릴게요.",
-//     "의\n알림 설정을 해제하실 건가요?",
-//     "알림 설정을 해제하시면 종료 알림을 받으실 수 없습니다.",
-//   ],
-//   DeviceState.disconnect: [
-//     "는\n연결이 끊겨서 사용할 수 없어요.",
-//     "를 사용해 주세요.\n빠른 시일 내에 연결하여 사용 가능하도록 하겠습니다.",
-//   ],
-//   DeviceState.breakdown: [
-//     "는\n고장으로 인해 사용할 수 없어요.",
-//     "를 사용해 주세요.\n빠른 시일 내에 수리하여 사용 가능하도록 하겠습니다.",
-//   ],
-// };
