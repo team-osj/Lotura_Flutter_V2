0652bb748e606b3f89656f81a95cbca4fe93ba34
유지우 <treeyu12345@naver.com>
2024-11-15 19:14:57 +0900

    ENCHANT :: 모드 별 스플래시 스크린 수정

--- lib/presentation/splash/view/splash_screen.dart
+++ lib/presentation/splash/view/splash_screen.dart
@@ -23,17 +23,23 @@ class _SplashScreenState extends ConsumerState<SplashScreen> {
     WidgetsBinding.instance.addPostFrameCallback((_) {
       /// 테마 설정 불러오기
       ref.read(getThemeOptionViewModelProvider.notifier).execute();
+
       /// splash 화면에서 메인 세탁실 설정을 불러옴
       ref.read(getLaundryRoomOptionViewModelProvider.notifier).execute();
+
       /// 앱이 빌드될 떄 서버 소켓과 연결
       ref.read(getStreamLaundryViewModelProvider.notifier).getLaundryEvent();
+
       /// 모든 장치의 상태를 GET 요청함
       ref.read(getStreamLaundryViewModelProvider.notifier).getAllLaundryList();
+
       /// 읽은 공지사항 리스트를 불러옴
       ref.read(getNoticeOptionViewModelProvider.notifier).execute();
+
       /// 공지사항 전체 리스트를 GET 요청
       ref.read(getNoticeViewModelProvider.notifier).execute();
-      Future.delayed(const Duration(milliseconds: 1100)).then((value) => context.go("/"));
+      Future.delayed(const Duration(milliseconds: 1100))
+          .then((value) => context.go("/"));
     });
   }
 
@@ -42,7 +48,7 @@ class _SplashScreenState extends ConsumerState<SplashScreen> {
     return LoturaLayout(
       child: Center(
         child: Image.asset(
-          "$imageCoreAsset/lotura_splash_image.png",
+          "$imageCoreAsset/lotura_${Theme.of(context).colorScheme.brightness.name}_splash_image.png",
           width: 280,
           height: 280,
         ),
