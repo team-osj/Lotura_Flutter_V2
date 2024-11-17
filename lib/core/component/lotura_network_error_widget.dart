36acfe4dfa62b13b6a0f7a92373a572dd58f1b82
유지우 <treeyu12345@naver.com>
2024-11-17 18:26:43 +0900

    ENCHANT :: 네트워크 에러 레이아웃을 위젯으로 분리

---
+++ lib/core/component/lotura_network_error_widget.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:lotura_v2/core/constants/lotura_style.dart';
+
+class LoturaNetworkErrorWidget extends StatelessWidget {
+  const LoturaNetworkErrorWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          SvgPicture.asset(
+            "$iconCoreAsset/network_error_icon.svg",
+            colorFilter: ColorFilter.mode(
+              Theme.of(context).colorScheme.surfaceContainerLow,
+              BlendMode.srcIn,
+            ),
+          ),
+          const SizedBox(height: 24),
+          Text(
+            "네트워크 연결이 끊겼습니다.",
+            style: LoturaTextStyle.heading4(
+              color: Theme.of(context).colorScheme.surfaceContainerLow,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/core/layout/lotura_network_error_layout.dart
+++ lib/core/layout/lotura_network_error_layout.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
-import 'package:lotura_v2/core/constants/lotura_style.dart';
+import 'package:lotura_v2/core/component/lotura_network_error_widget.dart';
 import 'package:lotura_v2/core/layout/lotura_layout.dart';
 
 class LoturaNetworkErrorLayout extends StatelessWidget {
@@ -15,32 +14,7 @@ class LoturaNetworkErrorLayout extends StatelessWidget {
   Widget build(BuildContext context) {
     return LoturaLayout(
       appBar: appBar,
-      child: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Column(
-              children: [
-                SvgPicture.asset(
-                  "$iconCoreAsset/network_error_icon.svg",
-                  colorFilter: ColorFilter.mode(
-                    Theme.of(context).colorScheme.surfaceContainerLow,
-                    BlendMode.srcIn,
-                  ),
-                ),
-                const SizedBox(height: 24),
-                Text(
-                  "네트워크 연결이 끊겼습니다.",
-                  style: LoturaTextStyle.heading4(
-                    color: Theme.of(context).colorScheme.surfaceContainerLow,
-                  ),
-                ),
-              ],
-            ),
-          ],
-        ),
-      ),
+      child: const LoturaNetworkErrorWidget(),
     );
   }
 }
